a560460a8f0dbca3075dbfccb01bf61219f48a6b
Joe Bernardi <joebernardi@hotmail.com>
2016-06-01 15:10:09 -0500

    finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  resources :users
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
