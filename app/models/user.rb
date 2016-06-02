ff24bbc373b8c8f3bd9e09c48d89cccc0a8b568a
Joe Bernardi <joebernardi@hotmail.com>
2016-06-02 12:48:27 -0500

    add email production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
 
-  attr_accessor :remember_token
-
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -28,6 +26,18 @@ class User < ActiveRecord::Base
   end
 
 
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
 
 
@@ -61,7 +71,10 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
 private
 
